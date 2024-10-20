80923f476cb09e1610637599189786d081b02faf
SENGOMZ24 <labellana_230000002074@uic.edu.ph>
2024-10-20 19:10:00 +0800

    UpdatedPortfolio

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-let darkModeIcon = document.querySelector('#darkMode-icon');
+// Menu Icon
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-darkModeIcon.onclick = () => {
-    darkModeIcon.classList.toggle('bx-sun');
-    document.body.classList.toggle('dark-mode');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+// Scroll Section Active Link
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top >=offset && top< offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*= '+id+']').classList.add('active');
+            })
+        }
+    });
     
-    if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
+
+
+// Stick Navigation Bar
+    let header = document.querySelector('.header');
+
+header.classList.toggle('sticky',window.scrollY>100);
+
+// Remove menu icon when navbar link is clicked
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark-mode');
-        darkModeIcon.classList.add('bx-sun');
-    }
-});
+// darklightmode
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = ()=>{
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode')
+}

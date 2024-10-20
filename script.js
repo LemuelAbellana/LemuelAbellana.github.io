f640eb21cedd9aca78c02075bf01f166405775ee
SENGOMZ24 <labellana_230000002074@uic.edu.ph>
2024-10-20 21:01:11 +0800

    UpdatedPortfolio

--- script.js
+++ script.js
@@ -48,3 +48,14 @@ darkModeIcon.onclick = ()=>{
     darkModeIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('dark-mode')
 }
+
+// Scroll-Reveal
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { orign: top });

3e71bc98d037c61a58b5c85ea93fb659be5d0a25
SENGOMZ24 <labellana_230000002074@uic.edu.ph>
2024-11-21 23:25:20 +0800

    Bug Fixes

--- script.js
+++ script.js
@@ -4,58 +4,54 @@ let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active')
-}
+    navbar.classList.toggle('active');
+};
 
 // Scroll Section Active Link
-
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () =>{
-    sections.forEach(sec=>{
+window.onscroll = () => {
+    let header = document.querySelector('.header');
+
+    // Sticky Navigation Bar
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    sections.forEach((sec) => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id')
+        let id = sec.getAttribute('id');
 
-        if(top >=offset && top< offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*= '+id+']').classList.add('active');
-            })
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((link) => {
+                link.classList.remove('active');
+                document
+                    .querySelector(`header nav a[href*='${id}']`)
+                    ?.classList.add('active');
+            });
         }
     });
-    
 
-
-// Stick Navigation Bar
-    let header = document.querySelector('.header');
-
-header.classList.toggle('sticky',window.scrollY>100);
-
-// Remove menu icon when navbar link is clicked
-
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
+    // Close Navbar on Scroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
-// darklightmode
-
+// Dark Light Mode
 let darkModeIcon = document.querySelector('#darkMode-icon');
 
-darkModeIcon.onclick = ()=>{
+darkModeIcon.onclick = () => {
     darkModeIcon.classList.toggle('bx-sun');
-    document.body.classList.toggle('dark-mode')
-}
+    document.body.classList.toggle('dark-mode');
+};
 
-// Scroll-Reveal
-
+// Scroll Reveal
 ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
-    delay:200
+    delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: top });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
